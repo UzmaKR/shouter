9e3e4f842637c19d999799118c70d1a3ba3626f5
Uzma Rahman <uzma.rahman@gmail.com>
2013-07-27 23:19:34 -0700

    workshop 1 : created basic app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Shouter::Application.config.secret_token = '6a9cf8e9dc92e9ce5d18238cd01250871f953d513dce8db530964b71fe7139941852c3e02575e3658a769c2b214267f60f95d29f5ec053d708403251275c6840'
